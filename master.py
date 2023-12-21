1ef8ffd65ee3d97724aa20ed671f042705136516
4brahamluna <149635644+4brahamluna@users.noreply.github.com>
2023-12-20 22:50:19 -0300

    Add files via upload

---
+++ master.py
@@ -0,0 +1,180 @@
+import streamlit as st
+import pandas as pd
+import requests
+import plotly.express as px
+import time
+
+st.set_page_config(layout = 'wide')
+
+def formata_numero(valor, prefixo= ' '):
+    for unidade in ['','mil']:
+        if valor < 1000:
+            return f'{prefixo} {valor:.2f} {unidade}'
+        valor /= 1000
+    return f'{prefixo} {valor:.2f} milhões'
+st.title('DASHBOARD DE VENDAS :shopping_trolley:')
+
+url = 'http://labdados.com/produtos'
+response = requests.get(url)
+dados = pd.DataFrame.from_dict(response.json())
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
+
+regioes = ['Brasil','Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul']
+st.sidebar.title('Filtros')
+regiao = st.sidebar.selectbox('Região', regioes)
+
+if regiao == 'Brasil':
+    regiao = ''
+
+todos_os_anos = st.sidebar.checkbox('Dados de todo o período', value=True)
+if todos_os_anos:
+    ano = ''
+else:
+    ano = st.sidebar.slider('Ano', 2020, 2023)
+
+query_string = {'regiao':regiao.lower(), 'ano':ano}
+response = requests.get(url, params = query_string)
+dados = pd.DataFrame.from_dict(response.json())
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
+
+filtro_vendedores = st.sidebar.multiselect('Vendedores', dados['Vendedor'].unique())
+if filtro_vendedores:
+    dados = dados[dados['Vendedor'].isin(filtro_vendedores)]
+
+#Ir na pasta
+#criar a pasta pages na pasta do projeto
+#colocar o arquivo dados_brutos.py dentro da pasta pages
+
+# Tabelas
+### Tabelas de Receita 
+receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
+receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
+
+receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq = 'M'))['Preço'].sum().reset_index()
+receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
+receita_mensal['Mês'] = receita_mensal['Data da Compra'].dt.month_name()
+
+receita_categorias = dados.groupby('Categoria do Produto')[['Preço']].sum().sort_values('Preço', ascending = False)
+
+### Tabelas venderoes 
+vendedores = pd.DataFrame(dados.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
+
+
+## Gráfico
+fig_mapa_receita = px.scatter_geo(receita_estados,
+                                  lat = 'lat',
+                                  lon = 'lon',
+                                  scope = 'south america',
+                                  size = 'Preço',
+                                  template = 'seaborn',
+                                  hover_name = 'Local da compra',
+                                  hover_data = {'lat':False, 'lon':False},
+                                  title = 'Receita por estado')
+
+fig_receita_mensal = px.line(receita_mensal,
+                             x = 'Mês',
+                             y = 'Preço',
+                             range_y = (0, receita_mensal.max()),
+                             color = 'Ano',
+                             line_dash = 'Ano',
+                             title = 'Receita Mensal')
+
+fig_receita_mensal.update_layout(yaxis_title = 'Receita')
+
+fig_receita_categorias = px.bar(receita_categorias,
+                                text_auto = True,
+                                title = 'Receita por categoria')
+
+fig_receita_estados = px.bar(receita_estados.head(),
+                             x = 'Local da compra',
+                             y = 'Preço',
+                             text_auto = True,
+                             title = 'Top estados (Receita)')
+
+fig_receita_estados.update_layout(yaxis_title = 'Receita')
+
+## Visualização do Streamlit
+aba1, aba2 = st.tabs(['Receita', 'Vendedores'])
+
+with aba1:
+    coluna1, coluna2 = st.columns(2)
+    with coluna1:
+        st.metric("Receita", formata_numero(dados['Preço'].sum(),'R$'))
+        st.plotly_chart(fig_mapa_receita, use_container_width=True)
+        st.plotly_chart(fig_receita_estados, use_container_width=True)
+    with coluna2:    
+        st.metric("Quantidade de vendas", formata_numero(dados.shape[0]))
+        st.plotly_chart(fig_receita_mensal, use_container_width = True)
+        st.plotly_chart(fig_receita_categorias, use_container_width = True)
+
+with aba2:
+    qtd_vendedores = st.number_input('Quantidade de vendedores', 2, 10, 5 )
+    coluna1, coluna2 = st.columns(2)
+    with coluna1:
+        st.metric("Receita", formata_numero(dados['Preço'].sum(),'R$'))
+        fig_receita_vendedores = px.bar(vendedores[['sum']].sort_values('sum', ascending = False).head(qtd_vendedores),
+                                        x = 'sum',
+                                        y = vendedores[['sum']].sort_values('sum', ascending = False).head(qtd_vendedores).index,
+                                        text_auto = True,
+                                        title = f'Top {qtd_vendedores} vendedores (receita)')
+        st.plotly_chart(fig_receita_vendedores)
+    with coluna2:    
+        st.metric("Quantidade de vendas", formata_numero(dados.shape[0]))
+
+
+st.dataframe(dados)
+
+@st.cache_data
+def converte_csv(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+def mensagem_sucesso():
+    sucesso = st.success('Arquivo baixado com sucesso', icon="✅")
+    time.sleep(5)
+    sucesso.empty()
+
+st.title('DADOS BRUTOS')
+
+url = 'https://labdados.com/produtos'
+
+response = requests.get(url)
+dados = pd.DataFrame.from_dict(response.json())
+dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
+
+with st.expander('Colunas'):
+    colunas = st.multiselect('Selecione as colunas', list(dados.columns), list(dados.columns))
+
+st.sidebar.title('Filtros')
+
+with st.sidebar.expander('Nome do produto'):
+    produtos = st.multiselect('Selecione os produtos', dados['Produto'].unique(), dados['Produto'].unique())
+with st.sidebar.expander('Preço do produto'):
+    preco = st.slider('Selecione os preço', 0, 5000, (0, 5000))
+with st.sidebar.expander('Data da compra'):
+    data_compra = st.date_input('Selecione a data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))
+
+query = '''
+Produto in @produtos and \
+@preco[0] <= Preço <= @preco[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1]
+'''
+
+dados_filtrados = dados.query(query)
+dados_filtrados = dados_filtrados[colunas]
+
+
+st.dataframe(dados_filtrados)
+
+st.markdown(f'A tabela possui :blue[{dados_filtrados.shape[0]}] linhas e :blue[{dados_filtrados.shape[1]}] colunas.')
+
+st.markdown('Escreva um nome para o arquivo')
+coluna1, coluna2 = st.columns(2)
+with coluna1:
+    nome_arquivo = st.text_input('', label_visibility= 'collapsed', value='dados')
+    nome_arquivo += '.csv'
+with coluna2:
+    st.download_button('Fazer o download da tabela em csv',
+                       data = converte_csv(dados_filtrados),
+                       file_name= nome_arquivo,
+                       mime = 'text/csv',
+                       on_click = mensagem_sucesso)
